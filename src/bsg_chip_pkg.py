e089a4306c2c20bdc7fc091b95f6a196f935b164
Dan Petrisko <petrisko@cs.washington.edu>
2024-05-15 11:29:40 -0500

    Adding TCL generation

---
+++ src/bsg_chip_pkg.py
@@ -0,0 +1,268 @@
+
+import argparse
+import math
+
+class Object(object):
+    pass
+
+# Global Tag Info. MUST correspond to the verilog or dead chips could result
+class BsgChipTagClients:
+    def __init__(self):
+        ##########################
+        ##### Initialize packages
+        ##########################
+        self.packages = Object()
+        self.packages.bsg_tag_pkg = "For bsg_tag_s"
+
+        ##########################
+        ##### Initialize parameters
+        ##########################
+        self.params = Object()
+        self.params.osc_num_rows = (
+                2,
+                "Number of rows for oscillator"
+                )
+        self.params.osc_num_cols = (
+                2,
+                "Number of cols for oscillator"
+                )
+        self.params.osc_num_taps = (
+                self.params.osc_num_rows[0] * self.params.osc_num_cols[0],
+                "Number of taps for oscillator"
+                )
+        self.params.osc_ctl_width = (
+                int(math.log2(self.params.osc_num_taps[0])),
+                "Width of control regs for oscillator"
+                )
+        self.params.osc_post_buf = (
+                2,
+                "Length of post oscillator buffer chain"
+                )
+        self.params.osc_ds_width = (
+                2,
+                "Width of oscillator downsampler"
+                )
+        self.params.dly_num_rows = (
+                2,
+                "Number of rows for delay line"
+                )
+        self.params.dly_num_cols = (
+                2,
+                "Number of cols for delay line"
+                )
+        self.params.dly_num_taps = (
+                self.params.dly_num_rows[0] * self.params.dly_num_cols[0],
+                "Number of taps for delay line"
+                )
+        self.params.dly_ctl_width = (
+                int(math.log2(self.params.dly_num_taps[0])),
+                "Width of control regs for delay line"
+                )
+        self.params.dly_post_buf = (
+                4,
+                "Length of post delay buffer chain"
+                )
+        self.params.dly_count_width = (
+                8,
+                "Width of the digital period approximater"
+                )
+
+        ##########################
+        ##### Initialize tag clients
+        ##########################
+        i = 0
+        self.clients = Object()
+
+        ##########################
+        ##### Oscillator Object Declaration
+        ##########################
+        self.clients.osc = Object()
+        self.clients.osc.ctl = (i,
+                self.params.osc_ctl_width[0],
+                "Programmable control for oscillator"
+                ); i+=1
+        self.clients.osc.trigger = (i,
+                1,
+                "Trigger for oscillator"
+                ); i+=1
+        self.clients.osc.ds = (i,
+                self.params.osc_ds_width[0]+1,
+                "Downsampler width"
+                ); i+=1
+        self.clients.osc.sel = (i,
+                2,
+                "Selection: 0=oscillator, 1=downsampler, 2=external, 3=zero"
+                ); i+=1
+
+        ##########################
+        ##### Delay Object Declaration
+        ##########################
+        self.clients.dly = Object()
+        self.clients.dly.unused = (i,
+                1,
+                "Unused placeholder"
+                ); i+=1
+
+        ##########################
+        ##### Monitor Object Declaration
+        ##########################
+        self.clients.mon = Object()
+        self.clients.mon.reset = (i,
+                1,
+                "Synchronous reset"
+                ); i+=1
+        self.clients.mon.sel = (i,
+                2,
+                "Selection: 0=osc, 1=dly"
+                ); i+=1
+
+    ###########################
+    #### Recurse through a field and yield 
+    ###########################
+    def print_param(self, param):
+        print(self.params.__dict__[param][0], end="")
+
+    def gen_packages(self, package):
+        for a, f in package.__dict__.items():
+            # Skip builtin
+            if '__' in a:
+                continue
+            # Param tuple
+            if isinstance(f, str):
+                yield (a, f)
+            else:
+                yield from self.gen_packages(f)
+
+    def gen_params(self, param):
+        for a, f in param.__dict__.items():
+            # Skip builtin
+            if '__' in a:
+                continue
+            # Param tuple
+            if isinstance(f, tuple):
+                yield (a, f)
+            else:
+                yield from self.gen_params(f)
+
+    def gen_clients(self, client):
+        for a, f in client.__dict__.items():
+            # Skip builtin
+            if '__' in a:
+                continue
+            # Client tuple
+            if isinstance(f, tuple):
+                yield a, f
+            else:
+                yield from self.gen_clients(f)
+
+    def gen_tgroups(self, group):
+        for a, f in group.__dict__.items():
+            # Skip builtin
+            if '__' in a:
+                continue
+            # Client tuple
+            if isinstance(f, tuple):
+                continue
+            else:
+                yield a, f
+                yield from self.gen_tgroups(f)
+
+    def print_verilog(self):
+        print("""
+package bsg_chip_pkg;
+  //////////////////////////////////////////////////////////////////////
+  //////// Packages
+  //////////////////////////////////////////////////////////////////////
+""", end="")
+        for p, v in self.gen_packages(self.packages):
+            print("""
+    // {comment}
+    import {pname}::*;
+    """.format(
+    comment = v,
+    pname = p
+), end="")
+        print("""
+  //////////////////////////////////////////////////////////////////////
+  //////// Parameters
+  //////////////////////////////////////////////////////////////////////
+  """, end="")
+        for p, v in self.gen_params(self.params):
+            print("""
+    // {comment}
+    localparam {pname} = {pvalue};
+    """.format(
+    comment = v[1],
+    pname = p,
+    pvalue = v[0]
+), end="")
+
+        print("""
+  //////////////////////////////////////////////////////////////////////
+  //////// Clients
+  //////////////////////////////////////////////////////////////////////
+  """, end="")
+        for p, t in self.gen_tgroups(self.clients):
+            print("""
+  typedef struct packed
+  {
+  """, end="")
+            for c, v in self.gen_clients(t):
+                print("""
+    // {comment}
+    // Width={width}
+    bsg_tag_s {name};
+    """.format(
+        comment = v[2],
+        width = v[1],
+        name = c
+        ), end="")
+        print("""
+  }} bsg_chip_{name}_tag_lines_s;
+  localparam bsg_chip_{name}_tag_local_els_gp =
+    $bits(bsg_chip_{name}_tag_lines_s) / $bits(bsg_tag_s);
+    """.format(
+        name = p
+        ), end="")
+        print("""
+  //////////////////////////////////////////////////////////////////////
+  //////// Warning: Danger Zone
+  //////// Must set parameters below correctly, else chip may fail
+  //////////////////////////////////////////////////////////////////////
+  typedef struct packed
+  {
+  """, end="")
+        for p, t in self.gen_tgroups(self.clients):
+            print("""
+    bsg_chip_{name}_tag_lines_s {name};
+""".format(
+    name = p
+), end="")
+        print("""
+  } bsg_chip_tag_lines_s;
+  localparam bsg_chip_tag_local_els_gp =
+    $bits(bsg_chip_tag_lines_s) / $bits(bsg_tag_s);
+
+endpackage
+
+""", end="")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog='bsg_chip_pkg.py',
+        description='Generates collateral for a bsg_chip',
+        epilog='Read script for more details'
+    )
+    parser.add_argument("--gentype", type=str, choices=["pkg", "param"], required=True, help="Which collateral")
+    parser.add_argument("--par", type=str, help="Parameter to inspect", required=False)
+
+    args = parser.parse_args()
+
+    x = BsgChipTagClients()
+    if args.gentype == "pkg":
+        x.print_verilog()
+    elif args.gentype == "param":
+        x.print_param(args.par)
+        
+
--- src/bsg_chip_pkg.sv
+++ src/bsg_chip_pkg.sv
@@ -1,56 +1,111 @@
 
 package bsg_chip_pkg;
-  import bsg_tag_pkg::*;
+  //////////////////////////////////////////////////////////////////////
+  //////// Packages
+  //////////////////////////////////////////////////////////////////////
 
-  // design parameters
-  localparam osc_num_taps_gp = 4;
-  localparam osc_ctl_width_gp = $clog2(osc_num_taps_gp);
-  localparam osc_ds_width_gp = 2;
-  localparam dly_num_taps_gp = 4;
-  localparam div_count_width_gp = 8;
-
-  // number of clients for the whole system
-  localparam tag_els_gp = 1024;
-  // id width for the whole system
-  localparam tag_lg_els_gp = $clog2(tag_els_gp);
-  // maximum payload width for the whole system
-  localparam tag_max_payload_width_gp = 12;
-  // number of bits required to represent the max payload width
-  localparam tag_lg_width_gp = $clog2(tag_max_payload_width_gp+1);
-
+    // For bsg_tag_s
+    import bsg_tag_pkg::*;
+    
+  //////////////////////////////////////////////////////////////////////
+  //////// Parameters
+  //////////////////////////////////////////////////////////////////////
+  
+    // Number of rows for oscillator
+    localparam osc_num_rows = 2;
+    
+    // Number of cols for oscillator
+    localparam osc_num_cols = 2;
+    
+    // Number of taps for oscillator
+    localparam osc_num_taps = 4;
+    
+    // Width of control regs for oscillator
+    localparam osc_ctl_width = 2;
+    
+    // Length of post oscillator buffer chain
+    localparam osc_post_buf = 2;
+    
+    // Width of oscillator downsampler
+    localparam osc_ds_width = 2;
+    
+    // Number of rows for delay line
+    localparam dly_num_rows = 2;
+    
+    // Number of cols for delay line
+    localparam dly_num_cols = 2;
+    
+    // Number of taps for delay line
+    localparam dly_num_taps = 4;
+    
+    // Width of control regs for delay line
+    localparam dly_ctl_width = 2;
+    
+    // Length of post delay buffer chain
+    localparam dly_post_buf = 4;
+    
+    // Width of the digital period approximater
+    localparam dly_count_width = 8;
+    
+  //////////////////////////////////////////////////////////////////////
+  //////// Clients
+  //////////////////////////////////////////////////////////////////////
+  
   typedef struct packed
   {
-    bsg_tag_s sel;
-    bsg_tag_s ds;
-    bsg_tag_s trigger;
+  
+    // Programmable control for oscillator
+    // Width=2
     bsg_tag_s ctl;
-  }  bsg_clk_osc_tag_lines_s;
-  localparam bsg_clk_osc_tag_local_els_gp =
-    $bits(bsg_clk_osc_tag_lines_s) / $bits(bsg_tag_s);
-
-  typedef struct packed
-  {
-    bsg_tag_s unused;
-  }  bsg_clk_dly_tag_lines_s;
-  localparam bsg_clk_dly_tag_local_els_gp =
-    $bits(bsg_clk_dly_tag_lines_s) / $bits(bsg_tag_s);
-
-  typedef struct packed
-  {
+    
+    // Trigger for oscillator
+    // Width=1
+    bsg_tag_s trigger;
+    
+    // Downsampler width
+    // Width=3
+    bsg_tag_s ds;
+    
+    // Selection: 0=oscillator, 1=downsampler, 2=external, 3=zero
+    // Width=2
     bsg_tag_s sel;
-    bsg_tag_s reset;
-  }  bsg_clk_mon_tag_lines_s;
-  localparam bsg_clk_mon_tag_local_els_gp =
-    $bits(bsg_clk_mon_tag_lines_s) / $bits(bsg_tag_s);
-
-  // Warning: Danger Zone
-  // Must set parameters below correctly, else chip may fail
+    
   typedef struct packed
   {
-    bsg_clk_mon_tag_lines_s mon;
-    bsg_clk_dly_tag_lines_s dly;
-    bsg_clk_osc_tag_lines_s osc;
-  }  bsg_chip_tag_lines_s;
+  
+    // Unused placeholder
+    // Width=1
+    bsg_tag_s unused;
+    
+  typedef struct packed
+  {
+  
+    // Synchronous reset
+    // Width=1
+    bsg_tag_s reset;
+    
+    // Selection: 0=osc, 1=dly
+    // Width=2
+    bsg_tag_s sel;
+    
+  } bsg_chip_mon_tag_lines_s;
+  localparam bsg_chip_mon_tag_local_els_gp =
+    $bits(bsg_chip_mon_tag_lines_s) / $bits(bsg_tag_s);
+    
+  //////////////////////////////////////////////////////////////////////
+  //////// Warning: Danger Zone
+  //////// Must set parameters below correctly, else chip may fail
+  //////////////////////////////////////////////////////////////////////
+  typedef struct packed
+  {
+  
+    bsg_chip_osc_tag_lines_s osc;
+
+    bsg_chip_dly_tag_lines_s dly;
+
+    bsg_chip_mon_tag_lines_s mon;
+
+  } bsg_chip_tag_lines_s;
   localparam bsg_chip_tag_local_els_gp =
     $bits(bsg_chip_tag_lines_s) / $bits(bsg_tag_s);
 
